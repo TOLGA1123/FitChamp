a43b1607daf2af34a824197a2b87ee0a5b35fbbd
Efe Tuna Can <91334996+Efe-Tuna-Can@users.noreply.github.com>
2024-05-03 17:58:21 +0300

    schema.sql updated

--- myapp/schema.sql
+++ myapp/schema.sql
@@ -22,10 +22,9 @@ CREATE TABLE IF NOT EXISTS trainer (
   Specialization varchar(40),
   Telephone_Number varchar(40),
   Social_Media varchar(100),
-  PRIMARY KEY (User_ID, Trainer_ID),
+  PRIMARY KEY (Trainer_ID),
   FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
 );
-
 
 CREATE TABLE IF NOT EXISTS trainee (
   User_ID char(11) PRIMARY KEY,
@@ -49,7 +48,7 @@ CREATE TABLE IF NOT EXISTS report (
 CREATE TABLE IF NOT EXISTS chat (
   Trainer_ID char(11),
   User_ID char(11),
-  Message_ID char(11) PRIMARY KEY,
+  Chat_ID char(11) PRIMARY KEY,
   FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
   FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
 );
@@ -118,7 +117,246 @@ CREATE TABLE IF NOT EXISTS workout_plan (
   Duration varchar(40),
   Difficulty_Level varchar(20),
   PRIMARY KEY (Routine_Name, Trainer_ID, User_ID),
-  FOREIGN KEY (Trainer_ID, User_ID) REFERENCES trainer (Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
   FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
 );
+
+
+-- 2.13 Group_Session
+CREATE TABLE IF NOT EXISTS Group_Session (
+  Trainer_ID char(11),
+  User_ID char(11),
+  Location varchar(40) NOT NULL,
+  Starting_Time varchar(40) NOT NULL,
+  End_Time varchar(40) NOT NULL,
+  Type varchar(20),
+  Max_Participants numeric(3,0) NOT NULL,
+  Price varchar(20) NOT NULL,
+  PRIMARY KEY (Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.14 Exercise
+CREATE TABLE IF NOT EXISTS Exercise (
+  Exercise_name varchar(20) PRIMARY KEY,
+  Description varchar(200) NOT NULL,
+  Muscle_Group_Targeted varchar(40) NOT NULL,
+  Equipment varchar(40) NOT NULL,
+  Difficulty_Level varchar(20)
+);
+
+-- 2.15 Cardio
+CREATE TABLE IF NOT EXISTS Cardio (
+  Exercise_name varchar(20) PRIMARY KEY,
+  Intensity_Level varchar(20) NOT NULL,
+  Cardio_Duration varchar(40) NOT NULL,
+  FOREIGN KEY (Exercise_name) REFERENCES Exercise (Exercise_name)
+);
+
+-- 2.16 HyperTrophy
+CREATE TABLE IF NOT EXISTS HyperTrophy (
+  Exercise_name varchar(20) PRIMARY KEY,
+  Number_of_Sets varchar(40) NOT NULL,
+  Rest_Durations varchar(40) NOT NULL,
+  FOREIGN KEY (Exercise_name) REFERENCES Exercise (Exercise_name)
+);
+
+-- 2.17 Forms
+CREATE TABLE IF NOT EXISTS Forms (
+  Exercise_name varchar(20),
+  Routine_name varchar(20),
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Exercise_name, Routine_name, Trainer_ID, User_ID),
+  FOREIGN KEY (Routine_name, Trainer_ID, User_ID) REFERENCES workout_plan (Routine_Name, Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+
+-- 2.18 Has
+CREATE TABLE IF NOT EXISTS Has (
+  Achievement_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Achievement_ID, User_ID),
+  FOREIGN KEY (Achievement_ID, User_ID) REFERENCES achievement (Achievement_ID, User_ID) ON DELETE CASCADE,
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID) ON DELETE CASCADE
+);
+
+-- 2.19 takes_up
+CREATE TABLE IF NOT EXISTS takes_up (
+  Routine_name varchar(20),
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Routine_name, Trainer_ID, User_ID),
+  FOREIGN KEY (Routine_name, Trainer_ID, User_ID) REFERENCES workout_plan (Routine_Name, Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.20 follows
+CREATE TABLE IF NOT EXISTS follows (
+  Nutrition_plan_name varchar(40),
+  User_ID char(11),
+  Trainer_ID char(11),
+  PRIMARY KEY (Nutrition_plan_name, User_ID, Trainer_ID),
+  FOREIGN KEY (Nutrition_plan_name, User_ID, Trainer_ID) REFERENCES nutrition_plan (Nutrition_plan_name, User_ID, Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID)
+);
+
+-- 2.21 aims_to
+CREATE TABLE IF NOT EXISTS aims_to (
+  Goal_ID char(11),
+  User_ID char(11),
+  Trainer_ID char(11),
+  PRIMARY KEY (Goal_ID, User_ID, Trainer_ID),
+  FOREIGN KEY (Goal_ID,User_ID,Trainer_ID) REFERENCES fitnessgoal (Goal_ID,User_ID,Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID)
+);
+
+-- 2.22 assigned
+CREATE TABLE IF NOT EXISTS assigned (
+  Routine_name varchar(20),
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Routine_name, Trainer_ID, User_ID),
+  FOREIGN KEY (Routine_name, Trainer_ID, User_ID) REFERENCES workout_plan (Routine_Name, Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.23 trains
+CREATE TABLE IF NOT EXISTS trains (
+  User_ID char(11),
+  Trainer_ID char(11),
+  PRIMARY KEY (User_ID, Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID)
+);
+
+-- 2.24 joins
+CREATE TABLE IF NOT EXISTS joins (
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.25 creates_session
+CREATE TABLE IF NOT EXISTS creates_session (
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.26 sends
+CREATE TABLE IF NOT EXISTS sends (
+  Trainer_ID char(11),
+  User_ID char(11),
+  Message_ID char(11),
+  PRIMARY KEY (Trainer_ID, User_ID, Message_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Message_ID, User_ID,Trainer_ID) REFERENCES message (Message_ID,User_ID,Trainer_ID)
+);
+
+-- 2.27 receives
+CREATE TABLE IF NOT EXISTS receives (
+  User_ID char(11),
+  Trainer_ID char(11),
+  Message_ID char(11),
+  PRIMARY KEY (User_ID, Trainer_ID, Message_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (Message_ID, User_ID,Trainer_ID) REFERENCES message (Message_ID,User_ID,Trainer_ID)
+);
+
+-- 2.28 made
+CREATE TABLE IF NOT EXISTS made (
+  Progress_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Progress_ID, User_ID),
+  FOREIGN KEY (Progress_ID,User_ID) REFERENCES progress (Progress_ID,User_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.29 monitor
+CREATE TABLE IF NOT EXISTS monitor (
+  Progress_ID char(11),
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Progress_ID, Trainer_ID, User_ID),
+  FOREIGN KEY (Progress_ID,User_ID) REFERENCES progress (Progress_ID,User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.30 contains
+CREATE TABLE IF NOT EXISTS contains (
+  Message_ID char(11),
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Message_ID, Trainer_ID, User_ID),
+  FOREIGN KEY (Message_ID, User_ID, Trainer_ID) REFERENCES message (Message_ID, User_ID, Trainer_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+
+-- 2.31 adds
+CREATE TABLE IF NOT EXISTS adds (
+  Nutrition_plan_name varchar(40),
+  User_ID char(11),
+  Trainer_ID char(11),
+  PRIMARY KEY (Nutrition_plan_name, User_ID, Trainer_ID),
+  FOREIGN KEY (Nutrition_plan_name, User_ID, Trainer_ID) REFERENCES nutrition_plan (Nutrition_plan_name, User_ID, Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID)
+);
+
+-- 2.32 plans
+CREATE TABLE IF NOT EXISTS plans (
+  Exercise_name varchar(20),
+  Trainer_ID char(11),
+  PRIMARY KEY (Exercise_name, Trainer_ID),
+  FOREIGN KEY (Exercise_name) REFERENCES Exercise (Exercise_name),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID)
+);
+
+-- 2.33 creates
+CREATE TABLE IF NOT EXISTS creates (
+  Routine_name varchar(20),
+  Trainer_ID char(11),
+  User_ID char(11),
+  PRIMARY KEY (Routine_name, Trainer_ID, User_ID),
+  FOREIGN KEY (Routine_name, Trainer_ID, User_ID) REFERENCES workout_plan (Routine_Name, Trainer_ID, User_ID),
+  FOREIGN KEY (Trainer_ID) REFERENCES trainer (Trainer_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID)
+);
+
+-- 2.34 creates_report
+CREATE TABLE IF NOT EXISTS creates_report (
+  User_ID char(11),
+  Report_ID char(11),
+  PRIMARY KEY (User_ID, Report_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Report_ID) REFERENCES report (Report_ID)
+);
+
+-- 2.35 overview
+CREATE TABLE IF NOT EXISTS overview (
+  User_ID char(11),
+  Report_ID char(11),
+  PRIMARY KEY (User_ID, Report_ID),
+  FOREIGN KEY (User_ID) REFERENCES "user" (User_ID),
+  FOREIGN KEY (Report_ID) REFERENCES report (Report_ID)
+);
+
+
 
