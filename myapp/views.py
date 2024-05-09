8674c1c663979db56bcefa8774ab18b0db2f3eae
Tolgahan Arslan <90002820+TOLGA1123@users.noreply.github.com>
2024-05-09 19:57:48 +0300

    ZXxZ

--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
 import uuid
-
+from django.contrib import messages
 
 
 def runschema(sql_file, cur):
@@ -47,67 +47,98 @@ def dictfetchone(cursor):
 
 def register(request):
     if request.method == 'POST':
-        # Generate unique user_id
+        # Generate unique user_id 
         user_id = generate_user_id()
 
         # Get form data
         user_name = request.POST.get('user_name')
         email = request.POST.get('email')
         password = request.POST.get('password')
-
+        role = request.POST.get('role')
+        if role == 'trainer':
+            #insert new trainer into trainer table (change html fields of trainer/ same ones with user-fields are already filled)
+            return render(request, 'trainer_register.html')
+        elif role == 'trainee':
+            #insert new trainee into trainee table
+            return render(request, 'trainee_register.html')
+        else:
+            return HttpResponse('Invalid role selected.')
+        #ALTER USER postgres WITH PASSWORD '1234';      #password = 1234 in pgadmin
+        #insert user after so avoid the possibility of only user part filled and go back
+        connection = psycopg2.connect(
+            dbname="mydatabase",
+            user="postgres",
+            password="1234",
+            host="localhost",
+            port="5432"
+        )
         # Execute raw SQL INSERT statement
         with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO userf (user_id, user_name, password, email) VALUES (%s, %s, %s, %s)", [user_id, user_name, password, email])
-            # Optionally, handle exceptions or validation
-
+            cursor.execute("SELECT COUNT(*) FROM userf WHERE Email = %s", [email])
+            count = cursor.fetchone()[0]
+            #User with the same email already exists
+            if count > 0:
+                messages.error(request, 'An account with this email already exists.')
+                return redirect('register')
+            cursor.execute("INSERT INTO userf (User_ID, User_name, Password, Email) VALUES (%s, %s, %s, %s)", [user_id, user_name, password, email])
+            connection.commit()
+            connection.close()
         return redirect('login')
     else:
         # Render registration form
+        messages.error(request, 'Registration failed')
         return render(request, 'register.html')
-
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
-
+        connection = psycopg2.connect(
+            dbname="mydatabase",
+            user="postgres",
+            password="1234",
+            host="localhost",
+            port="5432"
+        )
         # Execute raw SQL SELECT statement
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM userf WHERE User_name = %s", [username])      #email has unique constraint in db
+            cursor.execute("SELECT * FROM userf WHERE email = %s", [email])
             user_row = cursor.fetchone()
 
         if user_row:
             stored_password = user_row[2]  # Assuming password is stored in the third column
             if password == stored_password:  # Validate password
-                # User authenticated, redirect to home page
+                request.session['user_id'] = user_row[0]
                 return redirect('home')
             else:
-                # Invalid password, handle accordingly
-                pass
+                messages.error(request, 'Invalid password. Please try again.')
         else:
-            # User not found, handle accordingly
-            pass
-
+            messages.error(request, 'User not found. Please try again.')
+        connection.close()
     # Render login form
     return render(request, 'login.html')
 
-@login_required(login_url='/login/')
+
+#@login_required(login_url='/login/')
 def home(request):
-    with connection.cursor() as cursor:
-        # SQL query to retrieve all users
-        cursor.execute("SELECT * FROM userf")
-        users = cursor.fetchall()
+    if 'user_id' in request.session:
+        with connection.cursor() as cursor:
+            # SQL query to retrieve all users
+            cursor.execute("SELECT * FROM userf")
+            users = cursor.fetchall()
 
-        # SQL query to retrieve all trainers
-        cursor.execute("SELECT * FROM trainer")
-        trainers = cursor.fetchall()
+            # SQL query to retrieve all trainers
+            cursor.execute("SELECT * FROM trainer")
+            trainers = cursor.fetchall()
 
-    # Convert query results into a more manageable format, if necessary
-    user_list = [{'user_id': user[0], 'user_name': user[1], 'password': user[2], 'email': user[3]} for user in users]
-    trainer_list = [{'user_id': trainer[0], 'trainer_id': trainer[1], 'user_name': trainer[2], 'password': trainer[3], 'specialization': trainer[4], 'telephone_number': trainer[5], 'social_media': trainer[6]} for trainer in trainers]
+        # Convert query results into a more manageable format, if necessary
+        user_list = [{'user_id': user[0], 'user_name': user[1], 'password': user[2], 'email': user[3]} for user in users]
+        trainer_list = [{'user_id': trainer[0], 'trainer_id': trainer[1], 'user_name': trainer[2], 'password': trainer[3], 'specialization': trainer[4], 'telephone_number': trainer[5], 'social_media': trainer[6]} for trainer in trainers]
 
-    return render(request, 'home.html', {'users': user_list, 'trainers': trainer_list})
+        return render(request, 'home.html', {'users': user_list, 'trainers': trainer_list})
+    else:
+        return redirect('login')
 
-@login_required(login_url='/login/')
+#@login_required(login_url='/login/')
 def user_info(request, user_id):
     try:
         with connection.cursor() as cursor:
