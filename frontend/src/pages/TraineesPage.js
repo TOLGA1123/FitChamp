23ed66a60c6f9a26fa68cff0d31bdd17ce0f30b4
Tolgahan Arslan <90002820+TOLGA1123@users.noreply.github.com>
2024-05-18 16:41:17 +0300

    profiles last fix

--- frontend/src/pages/TraineesPage.js
+++ frontend/src/pages/TraineesPage.js
@@ -38,8 +38,9 @@ const TraineesPage = () => {
     return <div>Error loading user details</div>; // Display an error message if user details couldn't be fetched
   }
 
-  const handleTraineeSelect = (traineeId) => {
-    history.push(`/trainee/${traineeId}`);
+  const handleTraineeSelect = (trainee_Id) => {
+    console.log('handleTraineeSelect called with traineeId:', trainee_Id); // Add lo
+    history.push(`/trainee/${trainee_Id}`);
   };
 
   const handleNewClient = () => {
@@ -89,7 +90,7 @@ const TraineesPage = () => {
       <Box sx={{ p: 3 }}>
         <Grid container spacing={2}>
           {Trainees.map((trainee) => (
-            <Grid item xs={12} sm={6} md={4} key={trainee.id} onClick={() => handleTraineeSelect(trainee.trainer_id)}>
+            <Grid item xs={12} sm={6} md={4} key={trainee.id} onClick={() => handleTraineeSelect(trainee.user_id)}>
               <Paper elevation={3} sx={{ p: 2, textAlign: 'center' }}>
                 <Avatar sx={{ width: 100, height: 100, margin: 'auto' }} />
                 <Typography>{trainee.user_name}</Typography>
