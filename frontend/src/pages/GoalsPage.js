42b885b6a90182a515652f885c3c53daddecef2a
Tolga Artun Koçak <artun.kocak@ug.bilkent.edu.tr>
2024-05-20 03:12:37 +0300

    Sort at backend (finally)

--- frontend/src/pages/GoalsPage.js
+++ frontend/src/pages/GoalsPage.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Typography, Paper, Grid, AppBar, IconButton, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
+import { Box, Typography, Paper, Grid, AppBar, IconButton, Menu, MenuItem, LinearProgress } from '@mui/material';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
+import SortIcon from '@mui/icons-material/Sort';
 import NavTabs from './NavTabs';
 axios.defaults.withCredentials = true;
 
@@ -13,9 +14,36 @@ const GoalsPage = () => {
   const [loading, setLoading] = useState(true);
   const history = useHistory();
   const [sortCriteria, setSortCriteria] = useState('endDate');
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const fetchGoals = (sort_by) => {
-    axios.get('http://localhost:8000/sort-goals/', { params: { sort_by } })
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+
+  useEffect(() => {
+    if (sortCriteria) {
+      fetchSortedGoals(sortCriteria);
+    }
+  }, [sortCriteria]);
+
+  useEffect(() => {
+    const updateGoalsAndFetch = async () => {
+      try {
+        await axios.get('http://localhost:8000/auto-update-goals/');
+        fetchSortedGoals(sortCriteria);
+      } catch (error) {
+        console.error('Error updating goals:', error.response ? error.response.data : 'Server did not respond');
+        setLoading(false);
+        if (error.response && error.response.status === 401) {
+          history.push('/login');
+        }
+      }
+    };
+    updateGoalsAndFetch();
+  }, [sortCriteria, history]);
+
+  const fetchGoals = () => {
+    axios.get('http://localhost:8000/goals/')
       .then(response => {
         setGoals(response.data);
         setLoading(false);
@@ -29,21 +57,20 @@ const GoalsPage = () => {
       });
   };
 
-  useEffect(() => {
-    const updateGoalsAndFetch = async () => {
-      try {
-        await axios.get('http://localhost:8000/auto-update-goals/');
-        fetchGoals(sortCriteria);
-      } catch (error) {
-        console.error('Error updating goals:', error.response ? error.response.data : 'Server did not respond');
+  const fetchSortedGoals = (sort_by) => {
+    axios.get('http://localhost:8000/sort-goals/', { params: { sort_by } })
+      .then(response => {
+        setGoals(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching sorted goals:', error.response ? error.response.data : 'Server did not respond');
         setLoading(false);
         if (error.response && error.response.status === 401) {
           history.push('/login');
         }
-      }
-    };
-    updateGoalsAndFetch();
-  }, [sortCriteria, history]);
+      });
+  };
 
   const handleGoalClick = (goalId) => {
     history.push(`/goal/${goalId.trim()}`);
@@ -66,23 +93,17 @@ const GoalsPage = () => {
   };
 
   const handleSortChange = (event) => {
-    setSortCriteria(event.target.value);
+    setSortCriteria(event.target.getAttribute('value'));
+    setAnchorEl(null);
   };
 
-  const sortGoals = (goals, criteria) => {
-    switch (criteria) {
-      case 'startDate':
-        return goals.sort((a, b) => new Date(a.start_date) - new Date(b.start_date));
-      case 'endDate':
-        return goals.sort((a, b) => new Date(a.end_date) - new Date(b.end_date));
-      case 'GoalName':
-        return goals.sort((a, b) => a.goal_name.localeCompare(b.goal_name));
-      default:
-        return goals;
-    }
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  const sortedGoals = sortGoals([...goals], sortCriteria);
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   if (loading) {
     return (
@@ -128,22 +149,28 @@ const GoalsPage = () => {
           <IconButton sx={{ position: 'absolute', right: 16 }} onClick={handleMSGClick}>
             <MessageIcon />
           </IconButton>
+          <IconButton sx={{ position: 'absolute', right: 80 }} color="inherit" onClick={handleMenuOpen}>
+            <SortIcon />
+          </IconButton>
         </Box>
       </AppBar>
 
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel>Sort By</InputLabel>
-        <Select value={sortCriteria} onChange={handleSortChange} label="Sort By">
-          <MenuItem value="startDate">Start Date</MenuItem>
-          <MenuItem value="endDate">End Date</MenuItem>
-          <MenuItem value="GoalName">Goal Name</MenuItem>
-        </Select>
-      </FormControl>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem value="startDate" onClick={handleSortChange}>Start Date</MenuItem>
+        <MenuItem value="endDate" onClick={handleSortChange}>End Date</MenuItem>
+        <MenuItem value="GoalName" onClick={handleSortChange}>Goal Name</MenuItem>
+        <MenuItem value="progress" onClick={handleSortChange}>Progress</MenuItem>
+      </Menu>
+
       <Grid container spacing={2}>
-        {sortedGoals.length ? (
-          sortedGoals.map((goal) => (
+        {goals.length ? (
+          goals.map((goal) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={goal.goal_id}>
-              <Paper sx={{ p: 2, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', margin: '8px' }} onClick={() => handleGoalClick(goal.goal_id)}>
+              <Paper sx={{ p: 2, height: '100%', margin: '8px' }} onClick={() => handleGoalClick(goal.goal_id)}>
                 <Typography variant="h6">Goal Name: {goal.goal_name}</Typography>
                 <Typography>Goal Type: {goal.goal_type}</Typography>
                 <Typography>Current Value: {goal.current_value}</Typography>
@@ -151,6 +178,11 @@ const GoalsPage = () => {
                 <Typography>Start Date: {goal.start_date}</Typography>
                 <Typography>End Date: {goal.end_date}</Typography>
                 <Typography>Status: {goal.achieved ? 'Achieved' : 'Not Achieved'}</Typography>
+                <Box sx={{ mt: 2 }}>
+                  <Typography>Progress</Typography>
+                  <LinearProgress variant="determinate" value={goal.progress} />
+                  <Typography>{goal.progress}%</Typography>
+                </Box>
               </Paper>
             </Grid>
           ))
